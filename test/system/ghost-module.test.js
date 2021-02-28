eb66871160b826ff8c1d9919d08d7b7d17edfe23
Alex Welcing <alexwelcing@gmail.com>
2021-02-28 12:41:01 -0500

    add test ghost module

--- test/system/ghost-module.test.js
+++ test/system/ghost-module.test.js
@@ -9,46 +9,46 @@ let nuxt;
 let addTemplate;
 
 const setupNuxt = async config => {
-  nuxt = new Nuxt(config);
-
-  // Spy addTemplate
-  addTemplate = nuxt.moduleContainer.addTemplate = jest.fn(nuxt.moduleContainer.addTemplate);
-
-  const build = new Builder(nuxt);
-
-  await build.validatePages();
-  await build.generateRoutesAndFiles();
-  await nuxt.listen(3000);
-};
-
-jest.setTimeout(10000);
-
-describe('Nuxt Ghost Module', () => {
-  beforeAll(async () => {
     nuxt = new Nuxt(config);
 
     // Spy addTemplate
     addTemplate = nuxt.moduleContainer.addTemplate = jest.fn(nuxt.moduleContainer.addTemplate);
 
-    await new Builder(nuxt).build();
+    const build = new Builder(nuxt);
+
+    await build.validatePages();
+    await build.generateRoutesAndFiles();
     await nuxt.listen(3000);
-  }, 60000);
+};
 
-  afterAll(async () => {
-    await nuxt.close();
-  });
+jest.setTimeout(10000);
 
-  test('SSR', async () => {
-    const html = await get('/');
-    expect(html).toContain('Welcome to Ghost');
-  });
+describe('Nuxt Ghost Module', () => {
+    beforeAll(async() => {
+        nuxt = new Nuxt(config);
 
-  test('CSR', async () => {
-    const window = await nuxt.renderAndGetWindow(url('/'));
+        // Spy addTemplate
+        addTemplate = nuxt.moduleContainer.addTemplate = jest.fn(nuxt.moduleContainer.addTemplate);
 
-    window.onNuxtReady(() => {
-      const html = window.document.body.innerHTML;
-      expect(html).toContain('Welcome to Ghost');
+        await new Builder(nuxt).build();
+        await nuxt.listen(3000);
+    }, 60000);
+
+    afterAll(async() => {
+        await nuxt.close();
     });
-  });
+
+    test('SSR', async() => {
+        const html = await get('/');
+        expect(html).toContain('Welcome to Ghost');
+    });
+
+    test('CSR', async() => {
+        const window = await nuxt.renderAndGetWindow(url('/'));
+
+        window.onNuxtReady(() => {
+            const html = window.document.body.innerHTML;
+            expect(html).toContain('Welcome to Ghost');
+        });
+    });
 });
